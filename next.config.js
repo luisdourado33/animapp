b8325e7e548357c3313f15fca05c26017280e1ad
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-07 15:20:07 -0400

    chore(conf): add next-with-less loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withLess = require('next-with-less')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withLess({
+  ...nextConfig,
+  lessLoaderOptions: {},
+})
