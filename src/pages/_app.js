fc2a10ae1909fd204e8a0474f26a9d290844bc44
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 15:53:48 -0400

    feat(test): wrap Spinner component to testing display

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/global.less'
 
 import Navbar from '../components/organisms/Navbar'
+import Spinner from '../components/molecules/Spinner'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Spinner isActive />
       <Navbar />
       <Component {...pageProps} />
     </>
