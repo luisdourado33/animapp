06306492f350f83beeb98ddd8a002bbcb8d348cd
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-07 21:25:45 -0400

    feat: add SearchButton component

---
+++ src/components/atoms/SearchButton/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { SearchOutlined } from '@ant-design/icons'
+
+import { Container } from './styles'
+
+export default function SearchButton() {
+  return (
+    <Container>
+      <SearchOutlined />
+    </Container>
+  )
+}
