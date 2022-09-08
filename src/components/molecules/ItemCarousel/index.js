e203396d347052efab5ee13378f7fa2ce77b4668
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 18:20:00 -0400

    feat(component): adjust in ItemCarousel component

--- src/components/molecules/ItemCarousel/index.js
+++ src/components/molecules/ItemCarousel/index.js
@@ -1,25 +1,30 @@
 import React from 'react'
-
+import { CalendarOutlined } from '@ant-design/icons'
 import {
   Container,
   ImageHighlight,
   Details,
   TitleDetails,
+  StartDate,
   BadgesRow,
 } from './styles'
 
 import CarouselBadge from '../../atoms/CarouselBadge'
 
-export default function ItemCarousel({ children }) {
+export default function ItemCarousel({ attributes }) {
   return (
     <Container>
-      <ImageHighlight />
+      <ImageHighlight bgImage={attributes.posterImage.original} />
       <Details>
         <BadgesRow>
           <CarouselBadge>Badge</CarouselBadge>
           <CarouselBadge>Badge</CarouselBadge>
         </BadgesRow>
-        <TitleDetails>Lorem Ipsum</TitleDetails>
+        <TitleDetails>{attributes.canonicalTitle}</TitleDetails>
+        <StartDate>
+          <CalendarOutlined className="date-icon" />
+          <span className="date">{attributes.startDate}</span>
+        </StartDate>
       </Details>
     </Container>
   )
--- src/components/molecules/ItemCarousel/styles.js
+++ src/components/molecules/ItemCarousel/styles.js
@@ -1,26 +1,27 @@
 import styled from 'styled-components'
 
-import bg from '../../../assets/images/backgrounds/highlight-hero-bg.jpg'
-
 export const Container = styled.div`
   display: flex;
   border-radius: 12px;
   background-color: #ffff;
   margin-bottom: 50px;
+  align-items: center;
 `
 export const ImageHighlight = styled.div`
   width: 60%;
   height: 400px;
   border-top-left-radius: 12px;
   border-bottom-left-radius: 12px;
-  background-image: url(${bg.src});
+  background-image: url(${({ bgImage }) => bgImage});
   background-position: center;
   background-repeat: no-repeat;
+  background-size: cover;
 `
 export const Details = styled.div`
   display: flex;
   padding: 15px;
-  padding-block: 50px;
+  width: 50%;
+  overflow: hidden;
   flex-direction: column;
 `
 export const BadgesRow = styled.div`
@@ -29,11 +30,24 @@ export const BadgesRow = styled.div`
   flex-wrap: wrap;
 `
 export const TitleDetails = styled.h1`
+  font-size: 40px;
+  line-height: 45px;
   font-weight: bolder;
   transition: all 0.5s;
 
   &:hover {
     color: rgba(19, 101, 255, 0.8);
     cursor: pointer;
+  }
+`
+
+export const StartDate = styled.div`
+  display: flex;
+  align-items: center;
+  .date-icon {
+    color: rgba(19, 101, 255, 0.8);
+    margin-right: 10px;
+  }
+  .date {
   }
 `
