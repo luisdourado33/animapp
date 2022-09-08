908a1416bc68bc8c9668ad411387861008684681
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 15:50:48 -0400

    feat(component): add Brand component

--- src/components/atoms/Brand/styles.js
+++ src/components/atoms/Brand/styles.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
-import { Typography as AntdTypo } from 'antd'
 
-export const Container = styled.div``
-export const Typography = styled(AntdTypo.Title)`
+export const Container = styled.div`
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  flex-direction: column;
+  &:hover {
+    cursor: pointer;
+  }
+`
+export const Typography = styled.h1`
   color: #ffff;
+  font-weight: bolder;
+  padding: 0px;
+  margin: 0px;
+  transition: all 0.5s;
+
+  > span {
+    color: #000;
+  }
+
+  &:hover {
+    transform: scale(110%);
+  }
 `
