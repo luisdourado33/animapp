46b2be939f5dc113a9355176a25bb04fd896af66
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 15:55:21 -0400

    feat(component): add some components to Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import HighlightHero from '../components/organisms/HighlightHero'
+import AnimeListSection from '../components/organisms/AnimeListSection'
 
 export default function Home() {
-  return <HighlightHero />
+  return (
+    <>
+      <HighlightHero />
+      <AnimeListSection title="Mais indicados" buttonLabel="Ver todos" />
+    </>
+  )
 }
