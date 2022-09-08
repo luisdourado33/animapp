a997a51952c9cdeea90c5e02dcad97b67a73b8ac
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 15:53:06 -0400

    feat(component): modify HighlightHero component

--- src/components/organisms/HighlightHero/styles.js
+++ src/components/organisms/HighlightHero/styles.js
@@ -7,13 +7,11 @@ export const Container = styled.section`
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
-  height: 100%;
-  height: 500px;
+  height: fit-content;
 `
 export const Content = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
   background: rgba(19, 101, 255, 0.8);
   /* overflow: hidden; */
   padding-top: 64px;
