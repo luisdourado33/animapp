d46e58ee2bd50acbfc3037a8bfc4d56bf8da276b
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 16:20:48 -0400

    feat(api): add api headers

--- src/services/animeApi.js
+++ src/services/animeApi.js
@@ -1,5 +1,7 @@
 export default {
   baseURL: 'https://kitsu.io/api/edge',
-  Accept: 'application/vnd.api+json',
-  'Content-Type': 'application/vnd.api+json',
+  headers: {
+    Accept: 'application/vnd.api+json',
+    'Content-Type': 'application/vnd.api+json',
+  },
 }
