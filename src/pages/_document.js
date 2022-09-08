a6fa2b10046c5fc48a2a3115c4a85a9bd83c6b05
Luis Dourado <luis_dourado33@hotmail.com>
2022-09-08 18:42:00 -0400

    feat: remove crossorigin attr

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ class AppDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Bangers&display=swap"
             rel="stylesheet"
